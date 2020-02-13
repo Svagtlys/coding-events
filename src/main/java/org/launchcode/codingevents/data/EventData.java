67335695bbe87ea09f32bcb34d20e82bda9168f7
Svagtlys <45446348+Svagtlys@users.noreply.github.com>
2020-02-13 12:26:23 -0600

    Can remove events, implemented model binding

---
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventData {
+
+    //place to put events
+    private static final Map<Integer, Event> events = new HashMap<>();
+
+    //get all events
+    public static Collection<Event> getAll(){
+        return events.values();
+    }
+
+    //add an event
+    public static void add(Event event){
+        events.put(event.getId(), event);
+    }
+
+    //get one event
+    public static Event getById(Integer id){
+        return events.get(id);
+    }
+
+    //remove an event
+    public static void remove(Integer id){
+        if(events.containsKey(id)){
+            events.remove(id);
+        }
+    }
+
+}
